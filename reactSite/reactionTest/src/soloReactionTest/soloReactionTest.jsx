9e91c7477f6f763b6b7f6ccf9d5f5a133e83bdd4
ktm <maxbadhoc@gmail.com>
2025-05-28 22:40:12 +0200

    added back button to reaction test

--- reactSite/reactionTest/src/soloReactionTest/soloReactionTest.jsx
+++ reactSite/reactionTest/src/soloReactionTest/soloReactionTest.jsx
@@ -1,14 +1,29 @@
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import ReactionTest from '../reactionTestComponent/reactionTest';
 
 function SoloReactionTest(){
+    const navigate = useNavigate();
+
     let targetRuns = useParams().numberOfRuns;
+
+    const buttonStyle = {
+        width: '200px',
+        height: '50px',
+        fontSize: '16px',
+        margin: '10px',
+        cursor: 'pointer',
+        userSelect: 'none',
+    }
     
     return (
         <div>
             <h1>Simpler Reaktionstest</h1>
 
             <ReactionTest targetRuns={targetRuns}/>
+
+            <div className='reaction-test-buttons'>
+                <button style={buttonStyle} onClick={() => navigate('/')}>Zurück zur Startseite</button>
+            </div>
 
         </div>
     );
