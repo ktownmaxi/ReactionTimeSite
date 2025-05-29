1a7f2efca96b80c3be9538f236e398aa63ac31eb
ktm <maxbadhoc@gmail.com>
2025-05-29 15:49:01 +0200

    fixed double router bug

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,12 @@ import SoloReactionTest from './soloReactionTest/soloReactionTest';
 function App() {
 
   return (
-    <Router>
       <div>
         <Routes>
           <Route path="/" element={<StartPage/>}/>
           <Route path="/soloReactionTest/:numberOfRuns" element={<SoloReactionTest/>}/>
         </Routes>
       </div>
-    </Router>
 
   )
 }
